74d141671c13c801402ac501b18e0e134f9f842c
kieranvieira <kieranvieira@live.com>
2019-05-18 19:47:18 -0400

    Changed server start log

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./api/server.js');
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+    console.log(`\n*** Server running on http://localhost:${port} ***\n`);
 });
